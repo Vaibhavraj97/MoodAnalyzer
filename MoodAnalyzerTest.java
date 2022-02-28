e8cf19555f035465385678351972d82964373daf
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-02-28 09:05:48 +0530

    Added:

---
+++ MoodAnalyzerTest.java
@@ -0,0 +1,72 @@
+package com.bridgelabz.Exception;
+
+import com.bridgelabz.moodanalyzer.exception.MoodAnalysisException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import com.bridgelabz.moodanalyzer.model.MoodAnalyzer;
+public class MoodAnalyzerTest {
+    /*
+     *This Test Case Excepts
+     * Sad Mood
+     */
+    @Test
+    public void givenMessgae_WhenSadMoodShouldReturnSad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
+        String mood;
+        try {
+            mood = moodAnalyzer.analyseMood();
+            Assert.assertEquals("SAD", mood);
+        } catch (MoodAnalysisException e) {
+        }
+    }
+
+    /*
+     *This Test Case Excepts
+     * Any Other Mood
+     */
+    @Test
+    public void givenMessage_WhenNotInSadMoodShouldReturnHappy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any Mood");
+        String mood;
+        try {
+            mood = moodAnalyzer.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+        }
+
+    }
+
+    /*
+     *This Test Case Will Check For
+     * Null Pointer Exception
+     */
+    @Test
+    public void givenNullMood_ShouldReturnHappy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        String mood;
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect((Class<? extends Throwable>) MoodAnalysisException.class);
+            mood = moodAnalyzer.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     *This Test Case Will Check For
+     * Null Pointer Exception
+     * for empty and null values
+     */
+    @Test
+    public void givenNullMood_ShouldThrowException() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        try {
+            moodAnalyzer.analyseMood();
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.exceptionType.ENTERED_NULL, e.type);
+        }
+    }
+}
